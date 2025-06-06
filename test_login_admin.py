a699e82d8ddf736b5081215a28f67cc143845dd2
Lea Oddone <oddone.lea@gmail.com>
2025-06-06 16:20:32 +0200

    login_admin + config

--- test_login_admin.py
+++ test_login_admin.py
@@ -1,1 +1,7 @@
-.. .
+from config import SeleniumBrowser
+import time
+
+def login_admin ():
+    browser = SeleniumBrowser() #wait for the browser to be ready
+    browser.open_page("http://localhost:8000/admin/login/")
+    time.sleep(2)  # Wait for the page to load
