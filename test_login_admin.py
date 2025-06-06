49fad1c024f2c089e474972cfd2e402f47d54c87
Lea Oddone <oddone.lea@gmail.com>
2025-06-06 16:35:46 +0200

    + commentaires explicatifs

--- test_login_admin.py
+++ test_login_admin.py
@@ -2,6 +2,6 @@ from config import SeleniumBrowser
 import time
 
 def login_admin ():
-    browser = SeleniumBrowser() #wait for the browser to be ready
+    browser = SeleniumBrowser() #attend que le navigateur soit prêt
     browser.open_page("http://localhost:8000/admin/login/")
-    time.sleep(2)  # Wait for the page to load
+    time.sleep(2)  # attend que la page se charge
